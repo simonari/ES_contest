049a54ed27e818f793e42c485956287923394666
simonari <vsimonari@gmail.com>
2023-11-30 17:23:49 +0700

    Model of Room added

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128)
+    price = models.FloatField()
+    beds = models.PositiveIntegerField()
+    booked = models.BooleanField(default=False)
+    available_from = models.DateTimeField()
+
--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.db.models import Q
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Room
+from .serializers import RoomSerializer
+
+
+class GetRoomInfoView(APIView):
+    def get(self, request: Request):
+        p = request.query_params
+
+        price_from = p.get('price_from')
+        price_to = p.get('price_to')
+        beds_from = p.get('beds_from')
+        beds_to = p.get('beds_to')
+        available_from_from = p.get('available_from')
+        available_from_to = p.get('available_to')
+
+        query = Q()
+
+        if price_from:
+            query &= Q(price__gte=price_from)
+        if price_to:
+            query &= Q(price__lte=price_to)
+        if beds_from:
+            query &= Q(beds__gte=beds_from)
+        if beds_to:
+            query &= Q(beds__lte=beds_to)
+        if available_from_from:
+            query &= Q(available_from__gte=available_from_from)
+        if available_from_to:
+            query &= Q(available_from__lte=available_from_to)
+
+        queryset = Room.objects.filter(query)
+        serialized = RoomSerializer(queryset, many=True)
+        return Response(serialized.data)

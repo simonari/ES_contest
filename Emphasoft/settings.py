41b2303068c0aaf0962d293b7c0816b7c4479ae5
simonari <vsimonari@gmail.com>
2023-11-30 17:24:25 +0700

    Added DRF and app to apps list

--- Emphasoft/settings.py
+++ Emphasoft/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # REST
+    'rest_framework',
+    # APPS
+    'booking',
 ]
 
 MIDDLEWARE = [

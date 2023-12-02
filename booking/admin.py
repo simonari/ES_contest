8a1db860cbbf1df362f66a6d6a04224857c3b747
simonari <vsimonari@gmail.com>
2023-12-02 12:07:38 +0700

    Small changes to displayed fields

--- booking/admin.py
+++ booking/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Room
 
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    list_display = ("name", "price", "beds", "booked", "available_from",)
-    list_filter = ("beds", "booked",)
+    list_display = ['id', 'name', 'price', 'beds', 'booked', 'available_from', 'booked_by', ]
+    list_filter = ['beds', 'booked', ]

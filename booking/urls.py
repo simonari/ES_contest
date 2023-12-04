86327232b1da41d5664950a28747af9fef82adbf
simonari <vsimonari@gmail.com>
2023-12-04 16:01:09 +0700

    Separated endpoints for different actions

--- booking/urls.py
+++ booking/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('api/booking/', views.GetRoomInfoView.as_view()),
-    path('api/booking/<int:pk>', views.BookRoomView.as_view()),
+    path('api/booking/<int:pk>/', views.BookRoomView.as_view({"get": "retrieve"})),
+    path('api/booking/<int:pk>/book', views.BookRoomView.as_view({"patch": "partial_update"})),
     path('api/profile/booked/', views.BookedRoomListView.as_view()),
     path('api/profile', views.UserDetailView.as_view()),
     path('api/drf-auth/', include('rest_framework.urls')),

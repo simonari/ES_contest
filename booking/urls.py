5fc7c255fef346a7a861b11e4d77f1c4e9fabb5c
simonari <vsimonari@gmail.com>
2023-12-04 14:38:21 +0700

    Removed previous user registration, login e.t.c. endpoints. Added djoser ones

--- booking/urls.py
+++ booking/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path
+from django.urls import path, include, re_path
 
 from . import views
 
 urlpatterns = [
-    path('api/booking', views.GetRoomInfoView.as_view()),
+    path('api/booking/', views.GetRoomInfoView.as_view()),
     path('api/booking/<int:pk>', views.BookRoomView.as_view()),
+    path('api/profile/booked/', views.BookedRoomListView.as_view()),
     path('api/profile', views.UserDetailView.as_view()),
-    path('api/profile/booked', views.BookedRoomListView.as_view()),
-    path('api/register', views.UserRegisterView.as_view()),
-    path('api/login', views.UserLogInView.as_view()),
-    path('api/logout', views.UserLogOutView.as_view()),
+    path('api/drf-auth/', include('rest_framework.urls')),
+    re_path(r'^auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]

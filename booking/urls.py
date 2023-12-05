4889676934da22f8a1679cfc096baf33242152fd
simonari <vsimonari@gmail.com>
2023-12-05 15:37:13 +0700

    Small fix

--- booking/urls.py
+++ booking/urls.py
@@ -5,14 +5,14 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from . import views
 
 urlpatterns = [
-    path('api/booking/', views.RoomListView.as_view(), 'booking'),
+    path('api/booking/', views.RoomListView.as_view(), name='booking'),
     path('api/booking/<int:pk>/',
          views.RoomDetailView.as_view({"get": "retrieve"}),
-         'booking-room'),
+         name='booking-room'),
     path('api/booking/<int:pk>/book',
          views.RoomDetailView.as_view({"patch": "partial_update"}),
-         'booking-room-book'),
-    path('api/booking/booked/', views.RoomBookedListView.as_view(), 'booking-booked'),
+         name='booking-room-book'),
+    path('api/booking/booked/', views.RoomBookedListView.as_view(), name='booking-booked'),
     path('api/drf-auth/', include('rest_framework.urls')),
     path(r'api/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),

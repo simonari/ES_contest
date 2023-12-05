7223c9448ed3bfb2399adb14b4a0f738e369f5b4
simonari <vsimonari@gmail.com>
2023-12-05 15:06:00 +0700

    Give out names to endpoints

--- booking/urls.py
+++ booking/urls.py
@@ -5,10 +5,14 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from . import views
 
 urlpatterns = [
-    path('api/booking/', views.RoomListView.as_view()),
-    path('api/booking/<int:pk>/', views.RoomDetailView.as_view({"get": "retrieve"})),
-    path('api/booking/<int:pk>/book', views.RoomDetailView.as_view({"patch": "partial_update"})),
-    path('api/booking/booked/', views.RoomBookedListView.as_view()),
+    path('api/booking/', views.RoomListView.as_view(), 'booking'),
+    path('api/booking/<int:pk>/',
+         views.RoomDetailView.as_view({"get": "retrieve"}),
+         'booking-room'),
+    path('api/booking/<int:pk>/book',
+         views.RoomDetailView.as_view({"patch": "partial_update"}),
+         'booking-room-book'),
+    path('api/booking/booked/', views.RoomBookedListView.as_view(), 'booking-booked'),
     path('api/drf-auth/', include('rest_framework.urls')),
     path(r'api/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
